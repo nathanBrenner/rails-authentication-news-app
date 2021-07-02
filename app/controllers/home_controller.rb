f10be3ffdaffb28e3d9eab751128f564033c904f
Nathan Brenner <nathan.j.brenner@gmail.com>
2021-07-02 15:32:25 -0700

    user verification

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def index
+    p params
   end
+
+  def login
+    @params = params
+    if params['username']
+      user = User.find_by_username(params['username'])
+      @valid = user.authenticate(params['password'])
+    end
+
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :users
   get 'home/index'
+  get 'home/login'
+  post 'home/login'
+  post 'home/index'
   root 'home#index'
 end
